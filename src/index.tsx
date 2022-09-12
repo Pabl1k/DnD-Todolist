0e70a20abab74b0c5b6dc2b0ff389cf74cb4379e
Pabl1k <poiumazaya@gmail.com>
2022-09-12 16:16:36 +0300

    Connected Firebase to the project. Removed useless files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./api/config";
+import { IContext } from "./types/context";
 import Setup from "./Setup";
 import "./styles/index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const base = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const store = getFirestore(base);
+
+export const Context = createContext<IContext>({ auth, store });
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
-  <React.StrictMode>
+  <Context.Provider
+    value={{
+      auth,
+      store,
+    }}
+  >
     <Setup />
-  </React.StrictMode>
+  </Context.Provider>
 );
