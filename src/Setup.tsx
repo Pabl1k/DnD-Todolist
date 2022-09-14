f7a68332fb2826abc1870a2dbf6ff84e51c9e6d2
Pabl1k <poiumazaya@gmail.com>
2022-09-14 13:17:25 +0300

    Firebase version downgrade, login functionality, set routes

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,10 +1,27 @@
+import {Navigate, Route, Routes} from "react-router-dom";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useCustomContext} from "./hooks/useCustomContext";
 import MainPage from "./containers/MainPage/MainPage";
+import Login from "./components/Login/Login";
 import "./Setup.scss";
 
 const Setup = () => {
+  const { auth } = useCustomContext();
+  const [user] = useAuthState(auth);
+
   return (
     <div className="app-setup">
-      <MainPage />
+      {user ? (
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="*" element={<Navigate to={'/'} />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      )}
     </div>
   );
 };
